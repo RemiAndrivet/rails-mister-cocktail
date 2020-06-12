8717819752c34bbeb19df5522127d2ae03f16c8e
Remi <remi.andrivet@hotmail.com>
2020-06-12 15:10:37 +0200

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Cocktail.create(name: "Jagger Bomb")
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+# require 'json' #parse your url json into a ruby hash, not mandatory because you have it in the gems of rails
+require 'open-uri' #that allows ypou to open an url and read it
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+
+puts "1.Destroying ingredients"
+
+Ingredient.destroy_all
+
+puts "2. Opening JSON and parsing it into a ruby hash"
+json_string = open(url).read
+
+ruby_hash = JSON.parse(json_string)
+p ruby_hash
+
+puts "3.Creating ingredients"
+final_array = []
+ ruby_hash["drinks"].each do |ingredient_baby_hash|
+  final_array << Ingredient.create!(name: ingredient_baby_hash["strIngredient1"] )
+end
+
+final_array
+puts "4. finished"
+puts "5. You have #{Ingredient.count} ingredients seeded "
